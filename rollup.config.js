79a973b35ac35f88387283ac860ccaa537889068
Tim Roßkamp <timrosskamp97@gmail.com>
2018-05-19 21:09:55 +0200

    installed & configured babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import includePaths from 'rollup-plugin-includepaths';
+import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'src/js/index.js',
@@ -22,6 +23,7 @@ export default {
                 '.js',
                 '.json'
             ]
-        })
+        }),
+        babel()
     ]
 }
