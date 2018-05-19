50aed126bb36540aaeb4b86e26165172bb78d2d3
Tim Roßkamp <timrosskamp97@gmail.com>
2018-05-19 16:30:40 +0200

    set up rollup config

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import includePaths from 'rollup-plugin-includepaths';
+
+export default {
+    input: 'src/js/index.js',
+    output: {
+        file: 'assets/js/app.bundle.js',
+        format: 'iife'
+    },
+    plugins: {
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        includePaths({
+            paths: [
+                'src/js'
+            ]
+        }),
+        resolve({
+            extensions: [
+                '.js',
+                '.json'
+            ]
+        })
+    }
+}
