a87b0f0d39b8524919c77091ab48d474af2cc214
Tim Roßkamp <mail@timrosskamp.de>
2020-06-02 19:29:56 +0200

    update gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,45 @@
-const gulp = require('gulp');
-const pump = require('pump');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
-const sassInlineSVG = require('sass-inline-svg');
-const postcss = require('gulp-postcss');
-const mqpacker = require('css-mqpacker');
-const pxtorem = require('postcss-pxtorem');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const gulp = require('gulp')
+const pump = require('pump')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const sassInlineSVG = require('sass-inline-svg')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 gulp.task('sass', done => {
 	pump([
-		gulp.src(['src/scss/main.scss']),
-		concat('style.site.css'),
-		sass({
+		gulp.src('src/scss/style.scss', {
+			sourcemaps: true
+		}),
+		sass.sync({
+			outputStyle: 'expanded',
 			functions: {
 				svg: sassInlineSVG('./src/svg/', [])
 			}
 		}),
 		postcss([
-            mqpacker({
-                sort: true
-			}),
-			pxtorem({
-                rootValue: 16,
-                unitPrecision: 3,
-                propList: [
-                    'font',
-					'font-size',
-					'line-height',
-                    'letter-spacing'
-                ],
-                replace: true,
-                mediaQuery: true
-            }),
-			autoprefixer(),
-			cssnano()
+			autoprefixer()
         ]),
+		gulp.dest('dist/', {
+			sourcemaps: '.'
+		})
+	], done)
+});
+
+gulp.task('cssmin', done => {
+	pump([
+		gulp.src('dist/style.css'),
+		postcss([
+			cssnano()
+		]),
+		rename({
+			extname: '.min.css'
+		}),
 		gulp.dest('dist/')
-	], done);
-});
+	], done)
+})
 
-gulp.task('watch', () => {
-	gulp.watch(['src/scss/**'], gulp.series('sass'));
-});
-
-gulp.task('build', gulp.series('sass'));
+gulp.task('default', gulp.series('sass', 'cssmin', () => {
+	gulp.watch(['src/scss/**'], gulp.series('sass', 'cssmin'))
+}))
